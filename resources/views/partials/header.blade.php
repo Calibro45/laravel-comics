699c9252637caa063bde02c1614627ab9d05b0e7
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-04-19 15:12:50 +0200

    cartella img npm layout

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,15 @@
+<header class="main-header">
+    <div class="container">
+
+        <figure class="logo">
+            <img src="{{ asset('img/dc-logo.png') }}" alt="">
+        </figure>
+
+        <nav class="main-nav">
+            <ol>
+                
+            </ol>
+        </nav>
+
+    </div>
+</header>
