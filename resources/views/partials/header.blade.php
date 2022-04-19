7d5ff7bb2d6f3935215abca4560d97bae94bfda7
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-04-19 15:21:54 +0200

    nav link

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,48 @@
+@php 
+    $links = [
+        [
+            'href' => '#',
+            'text' => 'characters',
+        ],
+        [
+            'href' => '#',
+            'text' => 'comics',
+        ],
+        [
+            'href' => '#',
+            'text' => 'movies',
+        ],
+        [
+            'href' => '#',
+            'text' => 'tv',
+        ],
+        [
+            'href' => '#',
+            'text' => 'games',
+        ],
+        [
+            'href' => '#',
+            'text' => 'collectibles',
+        ],
+        [
+            'href' => '#',
+            'text' => 'videos',
+        ],
+        [
+            'href' => '#',
+            'text' => 'fans',
+        ],
+        [
+            'href' => '#',
+            'text' => 'news',
+        ],
+        [
+            'href' => '#',
+            'text' => 'shop',
+        ],
+    ]
+@endphp
+
 <header class="main-header">
     <div class="container">
 
@@ -7,9 +52,14 @@
 
         <nav class="main-nav">
             <ol>
-                
+                @foreach ($links as $link)
+                    <li>
+                        <a href="{{ $link['href'] }}">{{ $link['text'] }}</a>
+                    </li>
+                @endforeach
             </ol>
         </nav>
 
     </div>
 </header>
+
