d66d5fff220798179babd21ca1ec7a92a227eb3d
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-04-19 17:46:29 +0200

    comics card

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -5,9 +5,19 @@
 <section id="comics">
     <div class="container">
 
-        
+        <!-- start card comic -->
+        @foreach($comics as $comic)
+        <div class="comics-card-wrap">
+            <figure class="card-header">
+                <img src="{{ $comic['thumb'] }}" alt="">
+            </figure>
+            <div class="card-body">
+                <h4>{{ $comic['series'] }}</h4>
+            </div>
+        </div>
+        @endforeach
+        <!-- end card comic -->
                 
-
         <div class="comics-cta">
             <button>load more</button>
         </div>
