fc185f5d05982e6b8a2b0d9c38b829f177952795
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-04-20 16:37:50 +0200

    comic detail bozza

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -4,6 +4,8 @@
 
     <section id="banner-comic">
         <div class="container-md">
+            
+            {{-- card comics details --}}
 
             <div class="comic-wrap">
                 <figure>
@@ -12,6 +14,33 @@
                 <p class="comic book">comic book</p>
                 <p class="comic gallery">view gallery</p>
             </div>
+
+        </div>
+    </section>
+
+    <section id="comic-description">
+        <div class="container-md">
+
+            <div class="comic-desc-wrap">
+                <h2 class="comic-title">{{ $comic['title'] }}</h2>
+
+                <div class="price-wrap">
+                    <div>
+                        <h4>U.S. Price: {{ $comic['price'] }}</h4>
+                        <h4>available</h4>
+                    </div>
+                    <div>
+                        <h4>Chek Availability</h4>
+                    </div>
+                </div>
+
+                <p class="comic-description">{{ $comic['description'] }}</p>
+            </div>
+
+            <figure class="adversiment">
+                <img src="{{ asset('img/adv.jpg') }}" alt="adversiment">
+            </figure>
+
 
         </div>
     </section>
