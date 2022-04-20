bfbe194b6f12ae1b67747c1072a180f0da511520
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-04-20 18:10:17 +0200

    detail info

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -18,6 +18,8 @@
         </div>
     </section>
 
+    {{-- comic description --}}
+
     <section id="comic-description">
         <div class="container-md">
 
@@ -40,6 +42,55 @@
             <figure class="adversiment">
                 <img src="{{ asset('img/adv.jpg') }}" alt="adversiment">
             </figure>
+
+        </div>
+    </section>
+
+    {{-- comic info --}}
+
+    <section id="comic-info">
+        <div class="container-md">
+
+            <div class="col-6">
+                <h4 class="info-title">Talent</h4>
+                <ol>
+                    <li class="info-item">
+                        <p class="info-desc-title">Art by: </p>
+                        <p class="info-desc">
+                            @foreach($comic['artists'] as $artist)
+                                {{ $artist.', ' }} 
+                            @endforeach
+                        </p>
+                    </li>
+                    <li class="info-item">
+                        <p class="info-desc-title">Written by: </p>
+                        <p class="info-desc">
+                            @foreach($comic['writers'] as $writer)
+                                {{ $writer.', ' }}
+                            @endforeach
+                        </p>
+                    </li>
+                </ol>
+            </div>
+
+            <div class="col-6">
+                <h4 class="info-title">Specs</h4>
+                <ol>
+                    <li class="info-item">
+                        <p class="info-desc-title">Series: </p>
+                        <p class="info-desc uppercase">{{ $comic['series'] }}</p>
+                    </li>
+                    <li class="info-item">
+                        <p class="info-desc-title">U.S. Price: </p>
+                        <p class="info-desc">{{ $comic['price'] }}</p>
+                    </li>
+                    <li class="info-item">
+                        <p class="info-desc-title">On Sale Date</p>
+                        <p class="info-desc">{{ $comic['sale_date'] }}</p>
+                    </li>
+                </ol>
+
+            </div>
 
         </div>
     </section>
